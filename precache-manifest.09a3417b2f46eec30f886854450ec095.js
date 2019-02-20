bfaca6e6a323ed969cba8796d76217ef990f66b6
mjesiolowski <maciej.jesiolowski@gmail.com>
2019-02-20 10:46:47 +0100

    Updates

---
+++ precache-manifest.09a3417b2f46eec30f886854450ec095.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9417060d96d8e51aa59",
+    "url": "/React-ToDoApp/static/js/runtime~main.a9417060.js"
+  },
+  {
+    "revision": "1e28a60974a3d0669156",
+    "url": "/React-ToDoApp/static/js/main.1e28a609.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/React-ToDoApp/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "1e28a60974a3d0669156",
+    "url": "/React-ToDoApp/static/css/main.33e6331d.chunk.css"
+  },
+  {
+    "revision": "7fb90897270d80b0fc167258920848be",
+    "url": "/React-ToDoApp/index.html"
+  }
+];
